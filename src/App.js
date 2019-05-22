2a959781fbb7d882847a90c5181aec5adb9fb384
Masahiro Okada <masahiro.okada@modernworks.jp>
2019-05-22 18:22:34 +0900

    react fragment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const greeting = 'view variable';
+    const dom = <div className="foo">{greeting}</div>
     return (
-      <h1>
-        Hello World!!
-      </h1>
+        // <div>
+        <React.Fragment>
+          {dom}
+          <label htmlFor="bar">Bar : </label>
+          <input type="text" name="bar" id="bar"
+            onClick={ () => alert('message') }
+            onChange={ () => console.log('text changed')}
+          />
+        </React.Fragment>
+        // </div>
     );
   }
 }
