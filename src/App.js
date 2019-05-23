2d32d4ff3e3b8baba485e4e63a33f7b6d25705c0
Masahiro Okada <masahiro.okada@modernworks.jp>
2019-05-23 17:39:40 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-// クラスコンポーネント
-class App extends Component {
+const App = () => {
+  return (<Counter></Counter>)
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  handlePlus = () => {
+    this.setState({
+      count: this.state.count+1,
+    })
+  }
+  handleMinus = () => {
+    this.setState({
+      count: this.state.count-1,
+    })
+  }
+
   render() {
-    const profiles = [
-      { name: 'Taro', age: 10},
-      { name: 'Hanako', age: 15},
-      { name: 'NoName', age: 8}
-    ]
-
-    const greeting = 'view variable';
-    const dom = <div className="foo">{greeting}</div>
-
-    const users = profiles.map((profile, index) => {
-      return <User name={profile.name} age={profile.age} key={index}  />;
-    });
-
     return (
-        <React.Fragment>
-          {dom}
-          <label htmlFor="bar">Bar : </label>
-          <input type="text" name="bar" id="bar"
-            onClick={ () => alert('message') }
-            onChange={ () => console.log('text changed')}
-          />
-
-          {users}
-        </React.Fragment>
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlus}>+1</button>
+        <button onClick={this.handleMinus}>-1</button>
+      </React.Fragment>
     );
   }
 }
-
-// functionalコンポーネント
-const User = (props) => {
-  return <div>Hi, Iam {props.name}, and {props.age}years old.</div>;
-}
-
-// prop-types
-User.propTypes = {
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired,
-}
-
 
 export default App;
