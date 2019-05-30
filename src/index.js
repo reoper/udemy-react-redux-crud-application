de835976ddb2fd1827a1947f2c6e158aaad47187
Masahiro Okada <masahiro.okada@modernworks.jp>
2019-05-30 16:45:25 +0900

    implement new component

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/evenets/new" component={EventsNew} />
+        <Route exact path="/events/new" component={EventsNew} />
         <Route exact path="/" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,9 @@
  * 状態の管理を総括するオブジェクト 
  */
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import events from './events';
 
 export default combineReducers({
-    events
+    events, form
 })
