d0e25074aeb905692ed5c188436ae792d2cc4e53
Masahiro Okada <masahiro.okada@modernworks.jp>
2019-06-01 12:34:12 +0900

    一覧画面のスタイリング完了

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 
 const enhancer = process.env.NODE_ENV === 'development' ?
@@ -19,16 +20,18 @@ const enhancer = process.env.NODE_ENV === 'development' ?
 const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path="/events/new" component={EventsNew} />
-        <Route path="/events/:id" component={EventsShow} />
-        <Route exact path="/" component={EventsIndex} />
-        <Route exact path="/events" component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
+          <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>
   ,
   document.getElementById('root')
 );
